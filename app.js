cdad440d2b34eb883ad6686b7e376b38682ae32a
Geun Young Kee <edc1901@gmail.com>
2019-05-11 08:27:59 +0900

    graphql 추가

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-require('app-module-path').addPath(__dirname + '/util');
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const graphqlHTTP = require('express-graphql');
+// const Graphql = require('graphql');
+const {graphql, buildSchema} = require('graphql');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
+const bodyParserGQL = require('body-parser-graphql');
+const indexRouter = require('./routes/index');
+const DB = require('better-sqlite3-helper');
 
+DB({
+    path: 'data', // this is the default
+    memory: false, // create a db only in memory
+    readonly: false, // read only
+    fileMustExist: false, // throw error if database not exists
+    WAL: true, // automatically enable 'PRAGMA journal_mode = WAL'
+})
+const app = express();
 
-
-var indexRouter = require('./routes/index');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,26 +28,95 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParserGQL.graphql());
 
 app.use('/', indexRouter);
 
+
+const schema = buildSchema(`
+  type Query {
+    users: [User]
+    user(id:Int):[User]
+    posts: [Post]
+    post(id:Int): [Post]
+    comments: [Comment]
+    comment(id:Int): [Comment]
+  }
+  type User {
+    id: Int
+    name: String
+    create_date: String
+  }
+  type Post {
+    id: Int
+    title: String
+    text: String
+    author: Int
+    create_date: String
+  }
+  type Comment {
+    id: Int
+    post: Int
+    user: Int
+    text: String
+    create_date: String
+   }
+`);
+
+const root = {
+    users: () => {
+        const users = DB().query('SELECT * FROM User');
+        return users
+    },
+    user: (args, context, info) => {
+        const {id} = args
+        const user = DB().query('SELECT * FROM User where id=?',id);
+        return user
+    },
+    posts: ()=>{
+        const posts = DB().query('select * from Post');
+        return posts
+    },
+    post: (args, context, info)=>{
+        const {id} = args
+        const post = DB().query('select * from Post where id=?',id);
+        return post
+    },
+    comments: ()=>{
+        const comments = DB().query('select * from Comment');
+        return comments
+    },
+    comment: (args, context, info)=>{
+        const {id} = args
+        const comment = DB().query('select * from Comment where id=?',id);
+        return comment
+    }
+
+};
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var router = express.Router();
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('data');
-var moment = require('moment');
+const express = require('express');
+const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('data');
+const moment = require('moment');
 console.log(moment().format('YYYY-MM-DD h:m:s'))
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
-    var startPos =req.query.startPos ? req.query.startPos : 0;
-    var pageCount =req.query.pageCount ? req.query.pageCount : 10;
+    const startPos =req.query.startPos ? req.query.startPos : 0;
+    const pageCount =req.query.pageCount ? req.query.pageCount : 10;
     db.all("SELECT * FROM User limit ?, ?",
         [startPos, pageCount],
         function (err, row) {
@@ -15,7 +15,7 @@ router.get('/users', function (req, res, next) {
             res.json(row);
     })
 }).post('/users', function (req, res, next) {
-    var now = moment().format('YYYY-MM-DD HH:mm:ss')
+    const now = moment().format('YYYY-MM-DD HH:mm:ss')
     db.run("INSERT INTO User(name, create_date, update_date) Values(?,?,?)",
         [req.body.name, now, now],
         function (err) {
@@ -32,8 +32,8 @@ router.get('/users', function (req, res, next) {
 });
 
 router.get('/users/:id/posts', function (req, res, next) {
-    var startPos =req.query.startPos ? req.query.startPos : 0;
-    var pageCount =req.query.pageCount ? req.query.pageCount : 10;
+    const startPos =req.query.startPos ? req.query.startPos : 0;
+    const pageCount =req.query.pageCount ? req.query.pageCount : 10;
     db.all("SELECT * FROM Post where author=? limit ?,?",
         [req.params.id, startPos, pageCount],
         function (err, row) {
@@ -48,7 +48,7 @@ router.get('/users/:id/posts', function (req, res, next) {
             res.json(row)
     })
 }).put('/users/:id/posts/:postId', function (req, res, next) {
-    var now = moment().format('YYYY-MM-DD HH:mm:ss')
+    const now = moment().format('YYYY-MM-DD HH:mm:ss')
     db.run("update Post set title= ?, text = ?, update_date = ? where id= ? and author=?",
         [req.body.title, req.body.text, now, req.params.postId, req.params.id],
         function (err, row) {
@@ -63,7 +63,7 @@ router.get('/users/:id/posts', function (req, res, next) {
             res.json(`Row(s) delete: ${this.changes}`)
     })
 }).post('/users/:id/posts', function (req, res, next) {
-    var now = moment().format('YYYY-MM-DD HH:mm:ss')
+    const now = moment().format('YYYY-MM-DD HH:mm:ss')
     db.run("INSERT INTO Post(author,title, text, create_date, update_date) Values(?,?,?,?,?)",
         [req.params.id, req.body.title, req.body.title, now, now],
         function (err) {
@@ -73,8 +73,8 @@ router.get('/users/:id/posts', function (req, res, next) {
 });
 
 router.get('/users/:id/comments', function (req, res, next) {
-    var startPos =req.query.startPos ? req.query.startPos : 0;
-    var pageCount =req.query.pageCount ? req.query.pageCount : 10;
+    const startPos =req.query.startPos ? req.query.startPos : 0;
+    const pageCount =req.query.pageCount ? req.query.pageCount : 10;
     db.all("select * from Comment where Comment.user = ? limit ?,?",
         [req.params.id, startPos, pageCount],
         function (err, row) {
@@ -89,7 +89,7 @@ router.get('/users/:id/comments', function (req, res, next) {
             res.json(row);
     })
 }).post('/users/:id/posts/:postId/comments', function (req, res, next) {
-    var now = moment().format('YYYY-MM-DD HH:mm:ss')
+    const now = moment().format('YYYY-MM-DD HH:mm:ss')
     db.run(
         "INSERT INTO Comment(post,user, text, create_date, update_date) Values(?,?,?,?,?)",
         [req.params.postId,req.params.id, req.body.text, now, now],
@@ -98,7 +98,7 @@ router.get('/users/:id/comments', function (req, res, next) {
             res.json(`A row has been inserted with rowid ${this.lastID}`)
     })
 }).put('/users/:id/posts/:postId/comments/:commentsId', function (req, res, next) {
-    var now = moment().format('YYYY-MM-DD HH:mm:ss')
+    const now = moment().format('YYYY-MM-DD HH:mm:ss')
     db.run("update Comment set text = ?, update_date = ? where id= ??",
         [ req.body.text, now, req.params.commentId],
         function (err, row) {
@@ -115,8 +115,8 @@ router.get('/users/:id/comments', function (req, res, next) {
 });
 
 router.get('/posts/:postId/comments',function (req, res, next) {
-    var startPos =req.query.startPos ? req.query.startPos : 0;
-    var pageCount =req.query.pageCount ? req.query.pageCount : 10;
+    const startPos =req.query.startPos ? req.query.startPos : 0;
+    const pageCount =req.query.pageCount ? req.query.pageCount : 10;
     db.all("select * from Comment where post = ? limit ?,?",
         [req.params.postId,startPos,pageCount],
         function (err, row) {
